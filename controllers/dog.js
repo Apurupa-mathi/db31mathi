71c53c383e78ca78e3c162bac780f0bcfefaf840
Apurupa-mathi <apurupam97@gmail.com>
2021-11-12 12:07:02 -0600

    Updated controllers

--- controllers/dog.js
+++ controllers/dog.js
@@ -11,9 +11,16 @@ exports.dog_list = async function (req, res) {
     }
 };
 // for a specific dog.
-exports.dog_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: dog detail: ' + req.params.id);
-};
+exports.dog_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await dog.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
 // Handle dog create on POST.
 exports.dog_create_post = async function (req, res) {
     console.log(req.body)
@@ -39,9 +46,25 @@ exports.dog_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: dog delete DELETE ' + req.params.id);
 };
 // Handle dog update form on PUT.
-exports.dog_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: dog update PUT' + req.params.id);
-};
+exports.dog_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await dog.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.breed)  
+               toUpdate.breed = req.body.breed; 
+        if(req.body.gender) toUpdate.gender = req.body.gender; 
+        if(req.body.age) toUpdate.age = req.body.age; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
 
 // VIEWS
 // Handle a show all view
