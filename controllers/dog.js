9544c9f5217505cca2f22610722c7b93919870ad
Apurupa-mathi <apurupam97@gmail.com>
2021-11-17 23:23:53 -0600

    Added details changes

--- controllers/dog.js
+++ controllers/dog.js
@@ -11,16 +11,16 @@ exports.dog_list = async function (req, res) {
     }
 };
 // for a specific dog.
-exports.dog_detail = async function(req, res) { 
-    console.log("detail"  + req.params.id) 
-    try { 
-        result = await dog.findById( req.params.id) 
-        res.send(result) 
-    } catch (error) { 
-        res.status(500) 
-        res.send(`{"error": document for id ${req.params.id} not found`); 
-    } 
-}; 
+exports.dog_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await dog.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
 // Handle dog create on POST.
 exports.dog_create_post = async function (req, res) {
     console.log(req.body)
@@ -28,7 +28,7 @@ exports.dog_create_post = async function (req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"dog_type":"goat", "cost":12, "size":"large"}
     document.breed = req.body.breed;
     document.gender = req.body.gender;
     document.age = req.body.age;
@@ -42,29 +42,37 @@ exports.dog_create_post = async function (req, res) {
     }
 };
 // Handle dog delete form on DELETE.
-exports.dog_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: dog delete DELETE ' + req.params.id);
+exports.dog_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await dog.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
 };
 // Handle dog update form on PUT.
-exports.dog_update_put = async function(req, res) { 
+exports.dog_update_put = async function (req, res) {
     console.log(`update on id ${req.params.id} with body 
-${JSON.stringify(req.body)}`) 
-    try { 
-        let toUpdate = await dog.findById( req.params.id) 
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await dog.findById(req.params.id)
         // Do updates of properties 
-        if(req.body.breed)  
-               toUpdate.breed = req.body.breed; 
-        if(req.body.gender) toUpdate.gender = req.body.gender; 
-        if(req.body.age) toUpdate.age = req.body.age; 
-        let result = await toUpdate.save(); 
-        console.log("Sucess " + result) 
-        res.send(result) 
-    } catch (err) { 
-        res.status(500) 
+        if (req.body.breed)
+            toUpdate.breed = req.body.breed;
+        if (req.body.gender) toUpdate.gender = req.body.gender;
+        if (req.body.age) toUpdate.age = req.body.age;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
         res.send(`{"error": ${err}: Update for id ${req.params.id} 
-failed`); 
-    } 
-}; 
+failed`);
+    }
+};
 
 // VIEWS
 // Handle a show all view
@@ -78,3 +86,18 @@ exports.dog_view_all_Page = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+
+// Handle a show one view with id specified by query 
+exports.dog_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await dog.findById(req.query.id)
+        res.render('dogdetail',
+            { title: 'Dog Detail', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
--- routes/dog.js
+++ routes/dog.js
@@ -15,4 +15,6 @@ const dog_controlers= require('../controllers/dog');
 var router = express.Router();
 /* GET dogs */
 router.get('/', dog_controlers.dog_view_all_Page );
+/* GET detail dog page */ 
+router.get('/detail', dog_controlers.dog_view_one_Page); 
 module.exports = router;
