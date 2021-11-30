98775e4c465b8657c6bb89de6141b26406b55bd4
Apurupa-mathi <apurupam97@gmail.com>
2021-11-29 19:10:54 -0600

    Added bonus

--- models/dog.js
+++ models/dog.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const dogSchema = mongoose.Schema({
-breed: String,
+breed: {type:String, required:true},
 gender: String,
 age: { type: Number, min: 1, max: 30, default: 0 },
 })
--- routes/dog.js
+++ routes/dog.js
@@ -31,9 +31,9 @@ router.get('/', dog_controlers.dog_view_all_Page );
 /* GET detail dog page */ 
 router.get('/detail', dog_controlers.dog_view_one_Page); 
 /* GET create dog page */ 
-router.get('/create', dog_controlers.dog_create_Page); 
+router.get('/create',secured, dog_controlers.dog_create_Page); 
 /* GET create update page */ 
 router.get('/update',secured, dog_controlers.dog_update_Page); 
 /* GET create dog page */ 
-router.get('/delete', dog_controlers.dog_delete_Page); 
+router.get('/delete',secured, dog_controlers.dog_delete_Page); 
 module.exports = router;
